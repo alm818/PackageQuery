927ff94bb9663bd5881a25cc37601b32ba5a032d
alm818 <alm818@nyu.edu>
2023-11-03 16:37:17 +0400

    clean

---
+++ runnable/main.cpp
@@ -0,0 +1,12 @@
+#include "pb/util/udeclare.h"
+#include "pb/util/uconfig.h"
+#include "pb/util/udebug.h"
+#include "pb/det/synthetic.h"
+#include "pb/core/dual_reducer.h"
+#include "pb/core/gurobi_solver.h"
+
+using namespace pb;
+
+int main(){
+  cout << "OK" << endl;
+}
